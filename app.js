a41d871ce33480b0fdc88905a2e240666ccec011
lamisghanayem <>
2021-05-05 11:38:32 +0000

    java script

---
+++ app.js
@@ -0,0 +1,13 @@
+alert ('Welcome to our food factory!')
+
+var foodKind = prompt ('To check if your kind of food is available, add your kind of food')
+
+if (foodKind === 'chinese')
+{alert('Chinese food is available')}
+else if (foodKind === 'indian')
+{alert('Indian food is available')}
+else if (foodKind === 'italian')
+{alert('Italian food is available')}
+else
+{alert('Not available')}
+
